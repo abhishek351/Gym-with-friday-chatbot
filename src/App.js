2b916ab2dbe25dfa4b13e9816a258316379a4faa
abhishek351 <abhishek291013@gmail.com>
2023-07-28 00:46:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import "./App.css"
+import { Helmet } from 'react-helmet';
+const App = () => {
+  useEffect(() => {
+    const injectScript = document.createElement('script');
+    injectScript.src = 'https://cdn.botpress.cloud/webchat/v0/inject.js';
+    injectScript.async = true;
+    document.body.appendChild(injectScript);
 
-function App() {
+    return () => {
+      document.body.removeChild(injectScript);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="home-container">
+      <header className="header">
+        <h1>Welcome to Our Gym</h1>
       </header>
+      <main className="main">
+        <section className="section">
+          <h2>Train with the Best</h2>
+          <p>
+            Get in shape, build strength, and improve your skills with our
+            experienced instructors in martial arts, MMA, and boxing.
+          </p>
+          <button className="cta-btn">Learn More</button>
+        </section>
+      </main>
+      <footer className="footer">
+        <p>&copy; 2023 Gym Website. All rights reserved.</p>
+      </footer>
+      <Helmet>
+      <script
+          src="https://mediafiles.botpress.cloud/18c8c7d7-9dc4-4b06-9271-b0ed0dd315f4/webchat/config.js"
+          defer
+          
+        ></script>
+      </Helmet>
     </div>
   );
-}
+};
 
 export default App;
